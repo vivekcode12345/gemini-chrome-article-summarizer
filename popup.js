b006f0bc4c2448663dc31be4aa09ad403e63255d
Vivek Verma <vivek7148verma@gmail.com>
2026-07-16 17:03:54 +0530

    New Changed API endpoint from

--- popup.js
+++ popup.js
@@ -99,7 +99,7 @@ async function getGeminiSummary(text, summaryType, apiKey) {
 
   try {
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
